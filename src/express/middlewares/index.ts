9e5c2b9b2faea7fa2e40d73404c283d5a6b2eb0e
AlpoxDev <alpoxdev@gmail.com>
2022-03-21 21:33:23 +0900

    fix: Multer Middlewares

--- src/express/middlewares/index.ts
+++ src/express/middlewares/index.ts
@@ -4,5 +4,24 @@ import swagger from "./swagger";
 import test from "./test";
 import cors from "cors";
 import pagination from "./pagination";
+import createUploader, {
+  allUploader,
+  imageUploader,
+  videoUploader,
+  imageAndVideoUploader,
+} from "./multer";
 
-export { json, urlencoded, cors, jsonwebtoken, swagger, test, pagination };
+export {
+  json,
+  urlencoded,
+  cors,
+  jsonwebtoken,
+  swagger,
+  test,
+  pagination,
+  createUploader,
+  allUploader,
+  imageUploader,
+  videoUploader,
+  imageAndVideoUploader,
+};
