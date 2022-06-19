659c15b5af66b994766d67a069b3926207e2e8e7
AlpoxDev <alpoxdev@gmail.com>
2022-06-19 23:27:40 +0900

    husky, lint-staged test

--- src/openapi/constant.ts
+++ src/openapi/constant.ts
@@ -1,11 +1,11 @@
 export const OPEN_API_RESPONSES = {
-  200: "OK",
-  201: "Created",
-  204: "No Content",
-  400: "Bad Request",
-  401: "Unauthorized",
-  404: "Not Found",
-  500: "Server Internal Error",
+  200: 'OK',
+  201: 'Created',
+  204: 'No Content',
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  404: 'Not Found',
+  500: 'Server Internal Error',
 };
 
 console.log(`OPEN_API_RESPONSES: ${JSON.stringify(OPEN_API_RESPONSES)}`);
