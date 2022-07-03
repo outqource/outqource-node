b2a6ab916e0d032f341ec3f1a92e3ad212954ad8
박용준 <9898junjun2@gmail.com>
2022-07-03 23:50:34 +0900

    ADD : extra Date Method

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -74,3 +74,16 @@ export const getRecentYearStartEnd = (target: number) => {
     endAt,
   };
 };
+
+export const getDatesStartToLast = (startDate: string, lastDate: string): string[] | null => {
+  const regex = RegExp(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/);
+
+  if (!(regex.test(startDate) && regex.test(lastDate))) return null;
+  const result = [];
+  const curDate = new Date(startDate);
+  while (curDate <= new Date(lastDate)) {
+    result.push(curDate.toISOString().split('T')[0]);
+    curDate.setDate(curDate.getDate() + 1);
+  }
+  return result;
+};
