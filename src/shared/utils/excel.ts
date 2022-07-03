3d2e5efaa9d94a6504df051da7749fb87cd53f37
박용준 <9898junjun2@gmail.com>
2022-07-03 23:43:42 +0900

    ADD : Excel

---
+++ src/shared/utils/excel.ts
@@ -0,0 +1,65 @@
+import ExcelJS from 'exceljs';
+
+type TStatisticExcel = {
+  header: string;
+  data: Array<string>;
+  extra?: {
+    title: string;
+    headers: Array<string>;
+    extraData: Array<string>;
+  };
+};
+
+export const getDefaultExcelSheet = (
+  data: Record<string, any>,
+  convertHeader: (header: string) => string,
+): Array<TStatisticExcel> => {
+  return Object.entries(data).map(([key, value]) => {
+    if (typeof data[key] === 'object') {
+      const extraHeaders: string[] = [];
+      const extraData: string[] = [];
+      Object.entries(data[key]).forEach(([header, value]) => {
+        extraHeaders.push(convertHeader(header));
+        extraData.push(`${value}`);
+      });
+
+      return {
+        header: convertHeader(key),
+        data: [`${value}`],
+        extra: {
+          headers: extraHeaders,
+          extraData,
+          title: convertHeader(key),
+        },
+      };
+    } else
+      return {
+        header: convertHeader(key),
+        data: [`${value}`],
+      };
+  });
+};
+
+export const getExcelFile = async (rows: TStatisticExcel[]): Promise<ExcelJS.Buffer> => {
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet();
+
+  rows.forEach((row, index) => {
+    if (row.extra) {
+      const length = row.extra?.headers.length ?? 0;
+      row.extra.headers.forEach((header, headerIndex) => {
+        worksheet.getColumn(index * length + headerIndex - length).values = [
+          row.extra?.title,
+          header,
+          row.extra?.extraData[headerIndex],
+        ];
+      });
+
+      worksheet.mergeCells(1, index * length - length, 1, index * length - 1);
+    } else {
+      worksheet.getColumn(index + 1).values = [row.header, ...row.data];
+    }
+  });
+
+  return await workbook.xlsx.writeBuffer();
+};
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './parseValue';
 export * from './enum';
+export * from './excel';
 export * from './regex';
 export * from './dto';
 export * from './date';
